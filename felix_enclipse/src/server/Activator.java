ba0a78bd59a8e83784a76655a4d79f10d6c71d6c
jerrevds <jerrevdsande@gmail.com>
2012-10-19 20:59:36 +0200

    server verder klaarzetten, nog steeds classdeferror

--- felix_enclipse/src/server/Activator.java
+++ felix_enclipse/src/server/Activator.java
@@ -1,4 +1,4 @@
-package dosgi;
+package server;
 
 import java.util.Dictionary;
 import java.util.Hashtable;
@@ -10,17 +10,15 @@ import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceRegistration;
 
-public class DOSGiEchoactivator  implements BundleActivator {
+public class Activator  implements BundleActivator {
 	  private ServiceRegistration sr;
 
 	  public void start(BundleContext context) throws Exception { 
 	    Dictionary props = new Hashtable();
 	  
-	    props.put("osgi.remote.interfaces", "*");
-	//    props.put("osgi.remote.configuration.type", "org.apache.cxf.ws");
-	   // props.put("osgi.remote.configuration.pojo.address", "http://localhost:8888/dosgi");
-
-	//    props.put("osgi.remote.configuration.pojo.address", "<a class="linkclass" href="http://localhost:9090/oracle">http://localhost:9090/oracle</a>");
+	    props.put("service.exported.interfaces", "*");
+        props.put("service.exported.configs", "org.apache.cxf.ws");
+        props.put("org.apache.cxf.ws.address", "http://localhost:9090/dosgi");
 	    sr = context.registerService(EchoService.class.getName(), 
 	        new EchoServiceImpl(), props);
 	  }
