2410239ec22b871a37e00bccea0471e85997bd56
jerrevds <jerrevdsande@gmail.com>
2012-10-31 13:30:04 +0100

    beetje verder werken measuring

--- version2/androidclient/src/be/ugent/ods/osgi/tests/implementations/EchoTest.java
+++ version2/androidclient/src/be/ugent/ods/osgi/tests/implementations/EchoTest.java
@@ -7,6 +7,7 @@ import android.widget.TextView;
 import be.ugent.ods.osgi.protocolabstraction.ModuleAccessor;
 import be.ugent.ods.osgi.tests.interfaces.FeedbackInterface;
 import be.ugent.ods.osgi.tests.interfaces.TestInterface;
+import be.ugent.ods.osgi.tests.measure.MeasurementTool;
 import be.ugent.ods.testapplications.service.interfaces.EchoService;
 
 public class EchoTest implements TestInterface {
@@ -14,12 +15,15 @@ public class EchoTest implements TestInterface {
 	public void runTest(ModuleAccessor accessor, FeedbackInterface feedback) {
 		
 		EchoService echoservice = accessor.getModule(EchoService.class);
-		
+		MeasurementTool tool = new MeasurementTool();
+		tool.startMeasuring(1, feedback.getActivity());
 		String response = echoservice.echoString("print this!");
 		
 		//show the anwser
+
 		TextView text = new TextView(feedback.getActivity());
 		text.setText("answer was: "+response);
+		tool.stopMeasuring();
 		feedback.pushTestView(text);
 	}
 
--- version2/androidclient/src/be/ugent/ods/osgi/tests/measure/MeasurementTool.java
+++ version2/androidclient/src/be/ugent/ods/osgi/tests/measure/MeasurementTool.java
@@ -13,90 +13,124 @@ import android.net.TrafficStats;
 import android.os.Handler;
 
 public class MeasurementTool {
-private int iterations;
-private long start;
-private ApplicationInfo appinfo;
-private boolean started;
-private long dataR;
-private long dataT;
-private long packetsT;
-private long packetsR;
-private Handler memHandler;
-private int maxMemory;
-private int maxcpu;
-private Context context;
-private int memArb;
+	private int iterations;
+	private long start;
+	private ApplicationInfo appinfo;
+	private boolean started;
+	private long dataR;
+	private long dataT;
+	private long packetsT;
+	private long packetsR;
+	private int maxcpu;
+	private Context context;
+	private int memArb;
+	private int datapoints;
+	private int cumCpu;
+	private int cumMem;
+	private int cumMemArb;
+	private int maxMemArb;
+	private Thread thread;
 
-//todo placeholder for git
-	
+	// todo placeholder for git
 
-	public boolean startMeasuring(int iterations, Context context){	
+	public boolean startMeasuring(int iterations, Context context) {
 		this.iterations = iterations;
 		this.start = System.currentTimeMillis();
 		try {
-			appinfo = context.getPackageManager().getApplicationInfo(context.getPackageName(), PackageManager.GET_META_DATA);
+			appinfo = context.getPackageManager().getApplicationInfo(
+					context.getPackageName(), PackageManager.GET_META_DATA);
 		} catch (NameNotFoundException e) {
 			return false;
 		}
-		//only get data for current app, note that uid from app isn't perse unique but it will provide a minimum of filtering required
+		// only get data for current app, note that uid from app isn't perse
+		// unique but it will provide a minimum of filtering required
 		this.dataR = TrafficStats.getUidRxBytes(appinfo.uid);
 		this.packetsR = TrafficStats.getUidRxPackets(appinfo.uid);
 		this.dataT = TrafficStats.getUidTxBytes(appinfo.uid);
 		this.packetsT = TrafficStats.getUidTxPackets(appinfo.uid);
-		this.context=context;
+		this.context = context;
 		started = true;
-		memHandler = new Handler();
-		maxMemory = 0;
+		thread = new Thread(new memRunnable());
+		thread.start();
+		maxMemArb = 0;
 		maxcpu = 0;
-		ActivityManager am=(ActivityManager)context.getSystemService(Context.ACTIVITY_SERVICE);
-		android.os.Debug.MemoryInfo[] mem=am.getProcessMemoryInfo(new int[]{android.os.Process.myPid()});
+		datapoints = 0;
+		cumCpu = 0;
+		cumMem = 0;
+		cumMemArb = 0;
+		ActivityManager am = (ActivityManager) context
+				.getSystemService(Context.ACTIVITY_SERVICE);
+		android.os.Debug.MemoryInfo[] mem = am
+				.getProcessMemoryInfo(new int[] { android.os.Process.myPid() });
 		memArb = mem[0].getTotalPrivateDirty();
-		memHandler.postDelayed(new memRunnable(), 100);
 		return true;
 	}
-	
-	public void stopMeasuring(){
-		if(!started){
+
+	public void stopMeasuring() {
+		if (!started) {
 			throw new IllegalStateException("please fist start measuring");
 		}
-		long duration = System.currentTimeMillis()-start;
+		long duration = System.currentTimeMillis() - start;
 		this.dataR = TrafficStats.getUidRxBytes(appinfo.uid) - this.dataR;
-		this.packetsR = TrafficStats.getUidRxPackets(appinfo.uid) - this.packetsR ;
+		this.packetsR = TrafficStats.getUidRxPackets(appinfo.uid)
+				- this.packetsR;
 		this.dataT = TrafficStats.getUidTxBytes(appinfo.uid) - this.dataT;
-		this.packetsT = TrafficStats.getUidTxPackets(appinfo.uid) - this.packetsT;
-		ActivityManager am=(ActivityManager)context.getSystemService(Context.ACTIVITY_SERVICE);
-		android.os.Debug.MemoryInfo[] mem=am.getProcessMemoryInfo(new int[]{android.os.Process.myPid()});
-		memArb = mem[0].getTotalPrivateDirty() - memArb;
-		started=false;
+		this.packetsT = TrafficStats.getUidTxPackets(appinfo.uid)
+				- this.packetsT;
+		if(datapoints==0){
+			//avoid divide by zero in debug
+			datapoints=1;
+		}
+		int avgCpu = cumCpu / datapoints;
+		int avgMem = cumMem / datapoints;
+		int avgMemArb = cumMemArb / datapoints;
+
+		started = false;
+
 	}
-	
+
 	public class memRunnable implements Runnable {
 		public void run() {
-			//do measuring
+			// do measuring
+			while (started) {
+				ActivityManager am = (ActivityManager) context
+						.getSystemService(Context.ACTIVITY_SERVICE);
+				android.os.Debug.MemoryInfo[] mem = am
+						.getProcessMemoryInfo(new int[] { android.os.Process
+								.myPid() });
+				cumMemArb += mem[0].getTotalPrivateDirty() - memArb;
+				if (mem[0].getTotalPrivateDirty() - memArb > maxMemArb) {
+					maxMemArb = mem[0].getTotalPrivateDirty() - memArb;
 
-			 BufferedReader in = null;
+				}
+				BufferedReader in = null;
 
-		        try {
+				try {
 					Process process = null;
 					process = Runtime.getRuntime().exec("top -n 1 -d 1");
 
-		            in = new BufferedReader(new InputStreamReader(process.getInputStream()));
+					in = new BufferedReader(new InputStreamReader(
+							process.getInputStream()));
 
-		            String line ="";
-		            String content = "";
+					String line = "";
 
-		            while((line = in.readLine()) != null) {
-		            	content += line + "\n";
-		            }
-		            //in here make a fix to get memory usage and cpu usage
-		            System.out.println(content);
+
+					while ((line = in.readLine()) != null && !line.contains("be.ugent.ods.osgi")) {
+						
+					}
+					String[] data = line.split(" ");
+					int cpu =Integer.parseInt(data[6].replace("%", ""));
+					cumCpu+=cpu;
+					if(maxcpu < cpu){
+						maxcpu=cpu;
+					}
+					
 
 				} catch (IOException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
-				}
-				finally {
-					if(in != null) {
+				} finally {
+					if (in != null) {
 						try {
 							in.close();
 						} catch (IOException e) {
@@ -105,8 +139,12 @@ private int memArb;
 						}
 					}
 				}
-			if(started){
-				memHandler.postDelayed(this, 100);
+				try {
+					Thread.sleep(100);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 			}
 		}
 	}
