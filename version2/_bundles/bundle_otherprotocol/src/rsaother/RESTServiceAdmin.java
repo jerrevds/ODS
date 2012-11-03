e8f85b486f6ee114f3505027b8b32b5e13d6ffd8
Jeroen Penninck <jeroen@iRail.be>
2012-11-03 21:48:57 +0100

    Androidclient: updated id's  +  REST: implemented activate method...

--- version2/_bundles/bundle_otherprotocol/src/rsaother/RESTServiceAdmin.java
+++ version2/_bundles/bundle_otherprotocol/src/rsaother/RESTServiceAdmin.java
@@ -4,6 +4,7 @@ import java.util.Collection;
 import java.util.Map;
 
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.InvalidSyntaxException;
 import org.osgi.framework.ServiceReference;
 import org.osgi.service.remoteserviceadmin.EndpointDescription;
 import org.osgi.service.remoteserviceadmin.ExportReference;
@@ -11,6 +12,8 @@ import org.osgi.service.remoteserviceadmin.ExportRegistration;
 import org.osgi.service.remoteserviceadmin.ImportReference;
 import org.osgi.service.remoteserviceadmin.ImportRegistration;
 import org.osgi.service.remoteserviceadmin.RemoteServiceAdmin;
+import org.osgi.util.tracker.ServiceTracker;
+import org.osgi.util.tracker.ServiceTrackerCustomizer;
 
 import rsaother.exception.RESTException;
 
@@ -28,6 +31,37 @@ public class RESTServiceAdmin implements RemoteServiceAdmin {
 	public void activate() throws RESTException {
 		// -- do something here...
 		System.out.println("Hello there!");
+		
+		
+		/*
+		 * ServiceListener that automatically exports services with exported interfaces
+		 * as defined by the Remote Services specification
+		 */
+		try {
+			ServiceTracker serviceTracker = new ServiceTracker(context,
+				context.createFilter("(service.exported.interfaces=*)"), 
+				new ServiceTrackerCustomizer() {
+
+					@Override
+					public Object addingService(ServiceReference ref) {
+						Collection<ExportRegistration> regs = exportService(ref, null);
+						return regs;
+					}
+
+					@Override
+					public void modifiedService(ServiceReference ref,
+							Object regs) {}
+
+					@Override
+					public void removedService(ServiceReference ref,
+							Object regs) {
+						for(ExportRegistration r : (Collection<ExportRegistration>) regs){
+							r.close();
+						}
+					}
+				});
+			serviceTracker.open();
+		}catch(InvalidSyntaxException e){}
 	}
 
 	@Override
@@ -36,6 +70,7 @@ public class RESTServiceAdmin implements RemoteServiceAdmin {
 		// - we hebben een implementatie (in reference) van een service
 		// - de interface moeten we wel aangeraken (kan in properties)
 		// - die moeten we toevoegen aan de Restlet server...
+		System.out.println("We have a new endpoint!!! ID: "+reference.getProperty("service.id"));
 		
 		
 		// NOTE: return an object of the class RESTExportReference?
@@ -46,7 +81,7 @@ public class RESTServiceAdmin implements RemoteServiceAdmin {
 	public ImportRegistration importService(EndpointDescription endpoint) {
 		// Maak een importRegistration aan voor de service...
 		// HMMM... ?
-		System.out.println("We have a new endpoint!!! ID: "+endpoint.getServiceId());
+		System.out.println("Going to import a service!!! ID: "+endpoint.getServiceId());
 		
 		
 		// NOTE: return an object of the class RESTImportReference?
