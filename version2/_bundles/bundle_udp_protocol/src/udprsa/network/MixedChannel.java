2bdc9b781c567f3f196a44839175655f9bea7ad7
jerrevds <jerrevdsande@gmail.com>
2012-11-08 16:40:50 +0100

    fixed problem sending after each other, still problems in image

--- version2/_bundles/bundle_udp_protocol/src/udprsa/network/MixedChannel.java
+++ version2/_bundles/bundle_udp_protocol/src/udprsa/network/MixedChannel.java
@@ -18,7 +18,7 @@ import udprsa.network.api.NetworkChannel;
 import udprsa.network.message.ROSGiMessage;
 import udprsa.network.message.RemoteCallMessage;
 
-public class MixedChannel  implements NetworkChannel {
+public class MixedChannel implements NetworkChannel {
 
 	private MessageReceiver receiver;
 	private Socket tcpSocket;
@@ -30,19 +30,26 @@ public class MixedChannel  implements NetworkChannel {
 	private InetAddress ip;
 	private boolean connected = true;
 
-	public MixedChannel(DatagramSocket socketUDP, Socket socket, MessageReceiver receiver, InetAddress ip) throws IOException{
-		System.out.println("open channel:" + ip +"\n tcp: local port:" + socket.getLocalPort() +  " remote port: "+ socket.getPort() +"\n local ip: " + socket.getLocalSocketAddress().toString() +  " remote ip: " + socket.getRemoteSocketAddress().toString());
+	public MixedChannel(DatagramSocket socketUDP, Socket socket,
+			MessageReceiver receiver, InetAddress ip) throws IOException {
+		System.out.println("open channel:" + ip + "\n tcp: local port:"
+				+ socket.getLocalPort() + " remote port: " + socket.getPort()
+				+ "\n local ip: " + socket.getLocalSocketAddress().toString()
+				+ " remote ip: " + socket.getRemoteSocketAddress().toString());
 		this.receiver = receiver;
-		this.ip=ip;
+		this.ip = ip;
 		open(socketUDP, socket);
 		new TCPReceiverThread().start();
 		new UDPReceiverThread().start();
 	}
-	
-	public MixedChannel(DatagramSocket socketUDP, String ip, int port, MessageReceiver receiver) throws IOException{
-		this(socketUDP,new Socket(ip, port), receiver,InetAddress.getByName(ip));
-		System.out.println("open mixed channel: tcp port is" + port + " udp port is " + (port+1));
-		
+
+	public MixedChannel(DatagramSocket socketUDP, String ip, int port,
+			MessageReceiver receiver) throws IOException {
+		this(socketUDP, new Socket(ip, port), receiver, InetAddress
+				.getByName(ip));
+		System.out.println("open mixed channel: tcp port is" + port
+				+ " udp port is " + (port + 1));
+
 	}
 
 	private void open(DatagramSocket socketUDP, Socket socket) {
@@ -64,16 +71,16 @@ public class MixedChannel  implements NetworkChannel {
 		try {
 			tcpOutput = new ObjectOutputStream(new BufferedOutputStream(
 					tcpSocket.getOutputStream()));
-			
+
 			tcpOutput.flush();
-			tcpInput = new ObjectInputStream(new BufferedInputStream(tcpSocket
-					.getInputStream()));
+			tcpInput = new ObjectInputStream(new BufferedInputStream(
+					tcpSocket.getInputStream()));
 			System.out.println("tcp streams are set");
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 		this.udpSocket = socketUDP;
-		bos = new ByteArrayOutputStream(256);
+		bos = new ByteArrayOutputStream();
 		try {
 			udpOutput = new ObjectOutputStream(bos);
 			System.out.println("udp streams are set");
@@ -82,34 +89,60 @@ public class MixedChannel  implements NetworkChannel {
 		}
 	}
 
-
-
 	@Override
 	public String getRemoteAddress() {
-		return tcpSocket.getInetAddress().getHostAddress()+":"+tcpSocket.getPort();
+		return tcpSocket.getInetAddress().getHostAddress() + ":"
+				+ tcpSocket.getPort();
 	}
 
 	@Override
 	public String getLocalAddress() {
-		return tcpSocket.getLocalAddress().getHostAddress()+":"+tcpSocket.getLocalPort();
+		return tcpSocket.getLocalAddress().getHostAddress() + ":"
+				+ tcpSocket.getLocalPort();
 	}
 
 	@Override
 	public void sendMessage(ROSGiMessage message) throws IOException {
-		if(message instanceof RemoteCallMessage && (((RemoteCallMessage)message).isUDPEnabled())){
+		if (message instanceof RemoteCallMessage
+				&& (((RemoteCallMessage) message).isUDPEnabled())) {
 			System.out.println("send udp");
 			message.send(udpOutput);
 			byte[] buffer = bos.toByteArray();
-			DatagramPacket packet =	new DatagramPacket(buffer,buffer.length,ip,udpSocket.getLocalPort()); 
-			bos.reset();
+			
+			// first send length (code from
+			// http://stackoverflow.com/questions/3997459/send-and-receive-serialize-object-on-udp-in-java)
+			int number = buffer.length;
+			byte[] data = new byte[4];
+
+			// int -> byte[]
+			for (int i = 0; i < 4; ++i) {
+				int shift = i << 3; // i * 8
+				data[3 - i] = (byte) ((number & (0xff << shift)) >>> shift);
+			}
+			DatagramPacket packet = new DatagramPacket(data, 4, ip,
+					udpSocket.getLocalPort());
 			udpSocket.send(packet);
+
+			// then send payload
+			packet = new DatagramPacket(buffer, buffer.length, ip,
+					udpSocket.getLocalPort());
+
+			udpSocket.send(packet);
+			//reset for headers
+			bos = new ByteArrayOutputStream();
+			try {
+				udpOutput = new ObjectOutputStream(bos);
+				System.out.println("udp streams are reset");
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 			System.out.println("udp sended");
-		}else{
+		} else {
 			System.out.println("send tcp");
 			message.send(tcpOutput);
 			System.out.println("tcp sended");
 		}
-		
+
 	}
 
 	@Override
@@ -117,17 +150,14 @@ public class MixedChannel  implements NetworkChannel {
 		try {
 			tcpSocket.close();
 			System.out.println("tcp closed");
-		} catch(IOException ioe){
+		} catch (IOException ioe) {
 
 		}
 		// receiver.interrupt();
 		connected = false;
-		
+
 	}
-	
-	
-	
-	
+
 	class TCPReceiverThread extends Thread {
 		TCPReceiverThread() {
 			setDaemon(true);
@@ -141,8 +171,9 @@ public class MixedChannel  implements NetworkChannel {
 					receiver.receivedMessage(msg, MixedChannel.this);
 					System.out.println("receive tcp");
 				} catch (final IOException ioe) {
-					connected = false;
 					System.out.println("lost connection tcp");
+					ioe.printStackTrace();
+					connected = false;
 					try {
 						tcpSocket.close();
 					} catch (final IOException e1) {
@@ -155,8 +186,7 @@ public class MixedChannel  implements NetworkChannel {
 			}
 		}
 	}
-	
-	
+
 	class UDPReceiverThread extends Thread {
 		UDPReceiverThread() {
 			setDaemon(true);
@@ -165,8 +195,22 @@ public class MixedChannel  implements NetworkChannel {
 		public void run() {
 			while (connected) {
 				try {
-					byte[] buffer=new byte[1250];
-					DatagramPacket packet=new DatagramPacket(buffer,buffer.length);
+					// first receive length
+					byte[] data = new byte[4];
+					DatagramPacket packet = new DatagramPacket(data,
+							data.length);
+					udpSocket.receive(packet);
+
+					int len = 0;
+					// byte[] -> int
+					for (int i = 0; i < 4; ++i) {
+						len |= (data[3 - i] & 0xff) << (i << 3);
+					}
+
+					// now we know the length of the payload
+					byte[] buffer = new byte[len];
+					// then data
+					packet = new DatagramPacket(buffer, buffer.length);
 					udpSocket.receive(packet);
 					ByteArrayInputStream bin = new ByteArrayInputStream(packet.getData());
 					ObjectInputStream input = new ObjectInputStream(bin);
@@ -174,6 +218,7 @@ public class MixedChannel  implements NetworkChannel {
 					receiver.receivedMessage(msg, MixedChannel.this);
 					System.out.println("receive udp");
 				} catch (final IOException ioe) {
+					ioe.printStackTrace();
 					System.out.println("lost connection udp");
 					connected = false;
 					udpSocket.close();
