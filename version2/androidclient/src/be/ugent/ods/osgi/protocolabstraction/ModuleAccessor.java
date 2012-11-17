c1a078df32c5ac76d2cb5f9b8d7459a08d83f70b
jerrevds <jerrevdsande@gmail.com>
2012-11-17 12:09:31 +0100

    ip fix

--- version2/androidclient/src/be/ugent/ods/osgi/protocolabstraction/ModuleAccessor.java
+++ version2/androidclient/src/be/ugent/ods/osgi/protocolabstraction/ModuleAccessor.java
@@ -121,8 +121,8 @@ public class ModuleAccessor {
 		
 		//r-osgi-tim server
 		Map<String, Object> properties = new HashMap<String, Object>();
-		//properties.put("endpoint.id", "r-osgi://10.0.2.2:9278#"+rosgitim_ids.get(c.getName()));// TODO: put IP in property file or something like that (or in gui)
-		properties.put("endpoint.id", "r-osgi://192.168.2.5:9278#"+rosgitim_ids.get(c.getName()));// TODO: put IP in property file or something like that (or in gui)
+		properties.put("endpoint.id", "r-osgi://10.0.2.2:9278#"+rosgitim_ids.get(c.getName()));// TODO: put IP in property file or something like that (or in gui)
+		//properties.put("endpoint.id", "r-osgi://192.168.2.5:9278#"+rosgitim_ids.get(c.getName()));// TODO: put IP in property file or something like that (or in gui)
 		properties.put("service.imported.configs", "r-osgi");
 		properties.put("objectClass", new String[]{c.getName()});
 		EndpointDescription endpoint = new EndpointDescription(properties);
@@ -130,8 +130,8 @@ public class ModuleAccessor {
 		
 		//r-osgi-tim-udp server
 		properties = new HashMap<String, Object>();
-		//properties.put("endpoint.id", "r-osgi://10.0.2.2:9279#"+rosgitim_ids.get(c.getName()));// TODO: put IP in property file or something like that (or in gui)
-		properties.put("endpoint.id", "r-osgi://192.168.2.5:9279#"+rosgitim_ids.get(c.getName()));// TODO: put IP in property file or something like that (or in gui)
+		properties.put("endpoint.id", "r-osgi://10.0.2.2:9279#"+rosgitim_ids.get(c.getName()));// TODO: put IP in property file or something like that (or in gui)
+		//properties.put("endpoint.id", "r-osgi://192.168.2.5:9279#"+rosgitim_ids.get(c.getName()));// TODO: put IP in property file or something like that (or in gui)
 		properties.put("service.imported.configs", "r-osgi-udp");
 		properties.put("objectClass", new String[]{c.getName()});
 		endpoint = new EndpointDescription(properties);
@@ -139,8 +139,8 @@ public class ModuleAccessor {
 		
 		// other...
 		properties = new HashMap<String, Object>();
-	//	properties.put("endpoint.id", "http://10.0.2.2:8080/" + rosgitim_ids.get(c.getName()) + "/");
-		properties.put("endpoint.id", "http://192.168.2.5:8080/" + rosgitim_ids.get(c.getName()) + "/");
+		properties.put("endpoint.id", "http://10.0.2.2:8080/" + rosgitim_ids.get(c.getName()) + "/");
+		//properties.put("endpoint.id", "http://192.168.2.5:8080/" + rosgitim_ids.get(c.getName()) + "/");
 		properties.put("service.imported.configs", "r-osgi-other");
 		properties.put("objectClass", new String[]{c.getName()});
 		properties.put("interface", c);
